6b67b653d623b479d30f0d5b8762e77d9b510c11
amanndajosefiina <amanndavainikka@gmail.com>
2018-01-11 11:02:52 +0200

    task-book

---
+++ object-method/task-book/task-book/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_book
+{
+    class Person
+    {
+        //Muuttujat (fields) eli mitä olio tietää 
+        public string Name;
+        private int Age;
+
+        //Constructor, täytyy nimetä aina samalla nimellä kuin class eli luokka
+        public Person()
+        {
+            Name = "unknown";
+            Age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        //Methdos
+        public void GrowOld()
+        {
+            Age++;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Nimi: {Name}\nIkä: {Age}\nAikuinen: {IsAdult()}");
+        }
+
+        public bool IsAdult()
+        {
+            if (Age >= 18)
+                return true;
+            else
+                return false;
+            // return this age >= 18;  korvaa ylläolevan
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+    }
+}
---
+++ object-method/task-book/task-book/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task_book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personAmanda = new Person();
+            personAmanda.Name = "Amanda V";
+            personAmanda.GrowOld();
+            personAmanda.PrintPersonInfo();
+
+            Person newPerson = new Person("John Doe", 25);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
