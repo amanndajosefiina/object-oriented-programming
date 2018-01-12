39c4182396bc90d86e693b4266695f4a30da74dd
amanndajosefiina <amanndavainikka@gmail.com>
2018-01-12 18:07:57 +0200

    TaskCar

--- object-method/TaskCar/TaskCar/Program.cs
+++ object-method/TaskCar/TaskCar/Program.cs
@@ -8,16 +8,24 @@ namespace TaskCar
         {
             Car carAuto1 = new Car();
             carAuto1.AskData();
+            carAuto1.ShowCarInfo();
+            Console.WriteLine("Car speed after 30+:");
             carAuto1.Accelerate(30);
+            carAuto1.ShowCarInfo();
+            Console.WriteLine("Car speed after 10% slowdown:");
             carAuto1.Brake(90);
             carAuto1.ShowCarInfo();
-            
+                        
             Car carAuto2 = new Car();
             carAuto2.AskData();
+            carAuto2.ShowCarInfo();
+            Console.WriteLine("Car speed after 30+.");
             carAuto2.Accelerate(30);
+            carAuto2.ShowCarInfo();
+            Console.WriteLine("Car speed after 10% slowdown.");
             carAuto2.Brake(90);
             carAuto2.ShowCarInfo();
-           
+
             Console.ReadKey();
         }
     }
