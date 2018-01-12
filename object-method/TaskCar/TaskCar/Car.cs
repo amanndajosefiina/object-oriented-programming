a536592ca0e48aeeb2c97dfd19e69c620ba1aa06
amanndajosefiina <amanndavainikka@gmail.com>
2018-01-12 10:49:25 +0200

    TaskAuto

---
+++ object-method/TaskCar/TaskCar/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCar
+{
+    class Car
+    {
+        public string Brand;
+        public int Speed;
+
+        public Car()
+        {
+            Brand = "unknown";
+            Speed = 0;
+        }
+
+        public Car(string brand, int speed)
+        {
+            Brand = brand;
+            Speed = speed;
+        }
+
+        public void AskData()
+        {
+            Console.Write("Input car brand: ");
+            string userInput = Console.ReadLine();
+            Brand = userInput;
+
+            Console.Write("Input car speed: ");
+            string userInput1 = Console.ReadLine();
+            Speed = int.Parse(userInput1);           
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"\nCar brand: {Brand}\nCar speed: {Speed}\n");
+        }
+
+        public void Accelerate(int number)
+        {
+            Speed = Speed + number;
+        }
+
+        public void Brake(int number)
+        {
+            Speed = (Speed * number) / 100;
+        }
+    }
+}
---
+++ object-method/TaskCar/TaskCar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car carAuto1 = new Car();
+            carAuto1.AskData();
+            carAuto1.Accelerate(30);
+            carAuto1.Brake(90);
+            carAuto1.ShowCarInfo();
+            
+            Car carAuto2 = new Car();
+            carAuto2.AskData();
+            carAuto2.Accelerate(30);
+            carAuto2.Brake(90);
+            carAuto2.ShowCarInfo();
+           
+            Console.ReadKey();
+        }
+    }
+}
