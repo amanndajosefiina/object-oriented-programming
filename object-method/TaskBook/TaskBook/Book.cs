fe31c85be1300ea1fc0406ce4e0d9e8c4fc91382
amanndajosefiina <amanndavainikka@gmail.com>
2018-01-12 09:29:20 +0200

    TaskBook

---
+++ object-method/TaskBook/TaskBook/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskBook
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public int Id;
+        public int Price;
+
+        public Book()
+        {
+            Title = "unknown";
+            Author = "unknown";
+            Id = 0;
+            Price = 0;
+        }
+
+        public Book(string title, string author, int id, int price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Book's title: {Title}\nBook's author: {Author}\nBook's Id: {Id}\nBook's Price: {Price}\n");
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (this.Price > book.Price)
+                return ($"{this.Title} is more expensieve than {book.Title}.");
+            else
+                return ($"{book.Title} is more expensive than {this.Title}.");
+        }
+
+    }
+}
---
+++ object-method/TaskBook/TaskBook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaskBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookHarryPotter = new Book();
+            bookHarryPotter.Title = "Harry Potter";
+            bookHarryPotter.Author = "J.K. Rowling";
+            bookHarryPotter.Id = 1701552;
+            bookHarryPotter.Price = 20;
+
+            Book bookDorianGray = new Book();
+            bookDorianGray.Title = "Dorian Gray";
+            bookDorianGray.Author = "Oscar Wilde";
+            bookDorianGray.Id = 1701556;
+            bookDorianGray.Price = 25;
+
+            bookHarryPotter.PrintBookInfo();
+            bookDorianGray.PrintBookInfo();
+
+            Console.WriteLine($"{bookHarryPotter.CompareBook(bookDorianGray)}");
+
+            Console.ReadKey();
+    
+        }
+    }
+}
